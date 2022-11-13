5bccb6e080a51783c36fd6b65eaf42d4cf698aae
Vlada Shypalenko <lacuna2505@gmail.com>
2022-11-13 16:36:32 +0200

    M

--- cypress/support/helper.js
+++ cypress/support/helper.js
@@ -6,3 +6,49 @@ export function searchExistingProduct(productName){
 
     cy.get('h1.productname').should('contain', productName);
 }
+
+export function searchProduct(productName){
+    cy.get('#filter_keyword')
+    .type(productName)
+    .closest('form')
+    .submit();
+
+
+    cy.get(`ul.pagination a`).then(pages => {
+        return pages.length
+    }).then (pageCount => {
+        for (let i = 0; i < pageCount; i++){
+            cy.location().then(location => {
+                if(!location.search.includes('product/product')){
+                    cy.get('body').then(product =>{
+                        if(product.find(`a.prodocutname[title="${productName}"]`).length > 0){
+                            cy.get(`a.prodocutname[title="${productName}"]`).click
+                        } else {
+                            cy.get(`ul.pagination a`).contains('>').click();
+                        }
+                    })
+                }
+            })
+            
+        }
+    })
+
+    
+    
+}
+
+export function searchProductrecurtion(productName){
+    cy.get('#filter_keyword')
+    .type(productName)
+    .closest('form')
+    .submit();
+
+    cy.get('body').then(product =>{
+        if(product.find(`a.prodocutname[title="${productName}"]`).length > 0){
+            cy.get(`a.prodocutname[title="${productName}"]`).click
+        } else {
+            cy.get(`ul.pagination a`).contains('>').click();
+            searchProductrecurtion(productName)
+        }
+    })
+}
